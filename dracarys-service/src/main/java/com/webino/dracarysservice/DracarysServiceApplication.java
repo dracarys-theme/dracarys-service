f93c8a60a12391f7a9d0d79508c5108c1706ae2b
SELLAMI <Yassine.SELLAMI@dxc.com>
2019-05-28 10:23:35 +0000

    Init project

---
+++ dracarys-service/src/main/java/com/webino/dracarysservice/DracarysServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webino.dracarysservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DracarysServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DracarysServiceApplication.class, args);
+	}
+
+}
---
+++ dracarys-service/src/test/java/com/webino/dracarysservice/DracarysServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webino.dracarysservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DracarysServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
